0b590c38b572205677e9537b7797e35b0bba5368
kechin.vv <kechinv1@rambler.ru>
2021-05-29 19:04:11 +0300

    Header added fix

---
+++ heap.h
@@ -0,0 +1,21 @@
+//
+// Created by valer on 29.05.2021.
+//
+
+#ifndef UNTITLED_HEAP_H
+#define UNTITLED_HEAP_H
+
+
+void heapify(int *, int, int);
+
+void increase(int *, int, int);
+
+int *insert(int *, int, int);
+
+int *build(int *, int);
+
+void sort(int *, int);
+
+int extract_max(int *, int);
+
+#endif //UNTITLED_HEAP_H
